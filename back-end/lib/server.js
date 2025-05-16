ed17f5e1b5800afa93a4231e2e59f1538bca43d8
Gabriel Feltes dos Santos <gabrielfeltes.gg@hotmail.com>
2025-05-16 00:34:16 -0300

    test

--- back-end/lib/server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-console.log('MQTT_BROKER:', process.env.MQTT_BROKER_URL);
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
-
-// Inicie seu app normal aqui
-import './index.js';
